5e566d5d9dbd66ee45071d8c70a3a9ed620f4a92
Nikita <passpuffnikita@gmail.com>
2025-01-17 13:48:09 +0300

    update server configuration to allow connections from all hosts

--- dist/config/server.js
+++ dist/config/server.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = ({ env }) => ({
-    host: env('HOST', '206.81.26.245'),
+    host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1337),
     app: {
         keys: env.array('APP_KEYS'),

b9da407053a51bc91b47590f10df97e0304f4591
Nikita <passpuffnikita@gmail.com>
2025-01-20 18:35:04 +0300

    feat: add PUBLIC_URL configuration to server settings

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,9 @@
+import { url } from 'inspector';
+
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+	url: env('PUBLIC_URL', 'http://localhost:1337'),
   app: {
     keys: env.array('APP_KEYS'),
   },
